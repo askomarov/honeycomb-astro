b0315959cf6f7b79ea5266f3489c5d7689eb63ea
Артем Комаров <komarov.ar@office.lan>
2024-11-08 16:19:43 +0100

    feat(wip): добавляет пчелиные соты

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,6 @@
-import { initModals } from './modules/modals/init-modals'
-import { mobileVhFix } from './utils/mobile-vh-fix'
-import { initSlider } from './modules/init-slider'
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    console.log('Hello, world!');
 
-mobileVhFix()
-document.addEventListener(
-  'DOMContentLoaded',
-  () => {
-    initSlider()
-    // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-    // в load следует добавить скрипты, не участвующие в работе первого экрана
-    window.addEventListener('load', () => {
-      initModals()
-    })
-  },
-  true
-)
+  });
+});
